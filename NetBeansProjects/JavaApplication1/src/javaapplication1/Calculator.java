f0d771729d939409b27e1754b5f7dfe8e5f07c9a
FEBRIAN NATHAN MINTAREJA <i16020@ftis>
2018-11-02 13:27:20 +0700

    initial commit

---
+++ NetBeansProjects/JavaApplication1/src/javaapplication1/Calculator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author i16020
+ */
+public class Calculator {
+    public Calculator(){
+        
+    }
+    public float minus(float a,float b){
+        return 0;
+    }
+    public float plus(float a,float b){
+        return 0;
+    }
+    public float multiply(float a,float b){
+        return 0;
+    }
+    public float division(float a,float b){
+        return 0;
+    }
+}
